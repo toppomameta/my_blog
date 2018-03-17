9daf4704ebcf85e828b76e85c04696428288d661
toppomameta <toppo850122@gmail.com>
2018-03-17 15:31:31 +0900

    リファクタリング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,48 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
+
   def index
+    @posts = Post.order(created_at: :desc)
+    # 最新記事を５つ取得
+    @new_posts = Post.order(created_at: :desc).limit(5)
+
+    @author = Author.first
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params) # ストロングパラメータを引数に
+    @post.save # saveをしてデータベースに保存する。
+    redirect_to @post
   end
 
   def edit
   end
 
   def update
+    @post.update(post_params)
+    redirect_to @post
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path
   end
+
+  private
+
+  def post_params # ストロングパラメータを定義する
+    params.require(:post).permit(:title, :body, :category)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
