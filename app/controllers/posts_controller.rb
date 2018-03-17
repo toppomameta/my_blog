ca26750b94f9cf4f1bb8b22c5d4571d4699659f1
toppomameta <toppo850122@gmail.com>
2018-03-17 17:34:04 +0900

    スコープまでの設定分

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,9 @@ class PostsController < ApplicationController
   before_action :set_post, only: [ :show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.order(created_at: :desc)
-    # 最新記事を５つ取得
-    @new_posts = Post.order(created_at: :desc).limit(5)
-
+    @q = Post.order(created_at: :desc).ransack(params[:q])
+    @posts = @q.result.page(params[:page]).per(2)
+    @new_posts = Post.find_newest_article
     @author = Author.first
   end
 
